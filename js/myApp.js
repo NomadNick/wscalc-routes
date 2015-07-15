e737065ce3800f495cfc7ab6fbb8bd7680ef0469
NomadNick <emailnick123@gmail.com>
2015-07-15 11:15:04 +0700

    beautify all files

--- js/myApp.js
+++ js/myApp.js
@@ -2,32 +2,35 @@ var thisApp = angular.module('waitStaff', ['ngRoute', 'ngAnimate']);
 
 thisApp.config(['$routeProvider', function($routeProvider) {
   $routeProvider
-  .when('/home', {
-    templateUrl : 'views/home.html',
-    controller : 'HomeCtrl' })
+    .when('/home', {
+      templateUrl: 'views/home.html',
+      controller: 'HomeCtrl'
+    })
 
   .when('/new-meal', {
-    templateUrl : 'views/new-meal.html',
-    controller : 'NewMealCtrl' })
+    templateUrl: 'views/new-meal.html',
+    controller: 'NewMealCtrl'
+  })
 
   .when('/my-earnings', {
-    templateUrl : 'views/my-earnings.html',
-    controller : 'MyEarningsCtrl' })
+    templateUrl: 'views/my-earnings.html',
+    controller: 'MyEarningsCtrl'
+  })
 
   .otherwise({
-    redirectTo : '/home'
+    redirectTo: '/home'
   });
 }]);
 
 thisApp.run(function($rootScope, $timeout) {
   $rootScope.$on('$routeChangeStart', function() {
     $rootScope.isLoading = true;
-    });
+  });
   $rootScope.$on('$routeChangeSuccess', function() {
     $timeout(function() {
       $rootScope.isLoading = false;
-      }, 1000);
-    });
+    }, 1000);
+  });
 });
 
 thisApp.factory('Data', function() {
@@ -40,53 +43,6 @@ thisApp.controller('HomeCtrl', function($scope) {
 
 thisApp.controller('NewMealCtrl', function($scope) {
 
-    $scope.subtotal = 0;
-    $scope.tip = 0;
-    $scope.total = 0;
-    $scope.tip_total = 0;
-    $scope.meal_count = 0;
-    $scope.average_tip = 0;
-
-    $scope.submit = function() {
-      if($scope.myForm.$valid) {
-        console.log('The form is valid');
-        $scope.subtotal = $scope.meal_price + ($scope.meal_price * ($scope.tax_rate / 100));
-        $scope.tip = $scope.subtotal * ($scope.tip_percentage / 100);
-        $scope.total = $scope.subtotal + $scope.tip;
-        $scope.tip_total = $scope.tip + $scope.tip_total;
-        $scope.meal_count++;
-        $scope.average_tip = $scope.tip_total / $scope.meal_count;
-        $scope.meal_price = '';
-        $scope.tax_rate = '';
-        $scope.tip_percentage = '';
-
-      } else {
-        console.log('The form is invalid');
-      }
-    };
-
-    $scope.cancel = function() {
-      $scope.meal_price = '';
-      $scope.tax_rate = '';
-      $scope.tip_percentage = '';
-    }
-
-    $scope.reset= function() {
-      $scope.meal_price = '';
-      $scope.tax_rate= '';
-      $scope.tip_percentage = '';
-      $scope.subtotal = '';
-      $scope.tip = '';
-      $scope.total = '';
-      $scope.tip_total = '';
-      $scope.meal_count= '';
-      $scope.average_tip = '';
-    }
-
-});
-
-thisApp.controller('MyEarningsCtrl', function($scope) {
-
   $scope.subtotal = 0;
   $scope.tip = 0;
   $scope.total = 0;
@@ -95,7 +51,7 @@ thisApp.controller('MyEarningsCtrl', function($scope) {
   $scope.average_tip = 0;
 
   $scope.submit = function() {
-    if($scope.myForm.$valid) {
+    if ($scope.myForm.$valid) {
       console.log('The form is valid');
       $scope.subtotal = $scope.meal_price + ($scope.meal_price * ($scope.tax_rate / 100));
       $scope.tip = $scope.subtotal * ($scope.tip_percentage / 100);
@@ -118,15 +74,62 @@ thisApp.controller('MyEarningsCtrl', function($scope) {
     $scope.tip_percentage = '';
   }
 
-  $scope.reset= function() {
+  $scope.reset = function() {
     $scope.meal_price = '';
-    $scope.tax_rate= '';
+    $scope.tax_rate = '';
     $scope.tip_percentage = '';
     $scope.subtotal = '';
     $scope.tip = '';
     $scope.total = '';
     $scope.tip_total = '';
-    $scope.meal_count= '';
+    $scope.meal_count = '';
+    $scope.average_tip = '';
+  }
+
+});
+
+thisApp.controller('MyEarningsCtrl', function($scope) {
+
+  $scope.subtotal = 0;
+  $scope.tip = 0;
+  $scope.total = 0;
+  $scope.tip_total = 0;
+  $scope.meal_count = 0;
+  $scope.average_tip = 0;
+
+  $scope.submit = function() {
+    if ($scope.myForm.$valid) {
+      console.log('The form is valid');
+      $scope.subtotal = $scope.meal_price + ($scope.meal_price * ($scope.tax_rate / 100));
+      $scope.tip = $scope.subtotal * ($scope.tip_percentage / 100);
+      $scope.total = $scope.subtotal + $scope.tip;
+      $scope.tip_total = $scope.tip + $scope.tip_total;
+      $scope.meal_count++;
+      $scope.average_tip = $scope.tip_total / $scope.meal_count;
+      $scope.meal_price = '';
+      $scope.tax_rate = '';
+      $scope.tip_percentage = '';
+
+    } else {
+      console.log('The form is invalid');
+    }
+  };
+
+  $scope.cancel = function() {
+    $scope.meal_price = '';
+    $scope.tax_rate = '';
+    $scope.tip_percentage = '';
+  }
+
+  $scope.reset = function() {
+    $scope.meal_price = '';
+    $scope.tax_rate = '';
+    $scope.tip_percentage = '';
+    $scope.subtotal = '';
+    $scope.tip = '';
+    $scope.total = '';
+    $scope.tip_total = '';
+    $scope.meal_count = '';
     $scope.average_tip = '';
   }
 
